0e20e9c2aeae27bc1b546bf6a43e4c9394800e7f
Mundhe Dattatraya Dashrath <dattamundhe2410@gmail.com>
2025-01-27 19:26:22 +0530

    dict constructor

---
+++ Play with Python/Workshop/problem on dict.py
@@ -0,0 +1,9 @@
+# create dictionary using { }
+
+d1 = {1: 'Geeks', 2: 'For', 3: 'Geeks'}
+print(d1)
+
+# create dictionary using dict() constructor
+
+d2 = dict(a = "Geeks", b = "for", c = "Geeks")
+print(d2)
