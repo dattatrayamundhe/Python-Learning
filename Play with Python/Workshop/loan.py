e4f30ddb6c54e0c8a675fd37e4ef98067c377673
Mundhe Dattatraya Dashrath <dattamundhe2410@gmail.com>
2025-04-09 09:20:20 +0530

    Create loan.py

---
+++ Play with Python/Workshop/loan.py
@@ -0,0 +1,12 @@
+loan = int(input("Enter your loan amount : "))
+roi = float(input("Enter rate of interest : "))
+tennure = int(input("Enter the tenure in month : "))
+
+bundle = loan + (loan*0.02)
+
+interest = (bundle*roi)*(tennure*/12)
+
+
+
+
+
